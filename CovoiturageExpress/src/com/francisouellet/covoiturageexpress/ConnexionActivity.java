03d911cdc94e79bff144d72692b9dcf25959c0c4
Francis-Ouellet <franc.ouelle93@gmail.com>
2015-02-12 18:56:18 -0500

    Interfaces Connexion et Création compte, début

--- CovoiturageExpress/src/com/francisouellet/covoiturageexpress/ConnexionActivity.java
+++ CovoiturageExpress/src/com/francisouellet/covoiturageexpress/ConnexionActivity.java
@@ -1,27 +1,22 @@
-package com.francisouellet.covoiturageexpress.activities;
-
-import com.francisouellet.covoiturageexpress.R;
-import com.francisouellet.covoiturageexpress.R.id;
-import com.francisouellet.covoiturageexpress.R.layout;
-import com.francisouellet.covoiturageexpress.R.menu;
+package com.francisouellet.covoiturageexpress;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class ConnexionActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_connexion);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.connexion, menu);
 		return true;
 	}
 
